969a43245761ea4d7475a26c33900c306bab4910
AlekseyVY <alekseyVY@gmail.com>
2020-05-23 15:11:56 +0400

    exercises 5.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
+
+  //Gets all blogs at component render
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+      try {
+        const user = await loginService.login({username, password})
+
+          blogService.setToken(user.token)
+          setUser(user)
+          setUsername('')
+          setPassword('')
+      } catch (exception) {
+        console.log(exception)
+      }
+  }
+
+  //login form for logging in user with username and password
+  const loginForm = () => (
+      <form onSubmit={handleLogin}>
+        <div>
+          Username: <input type={'text'} value={username} name={'Username'} onChange={({target}) => setUsername(target.value)}/>
+        </div>
+        <div>
+          Password: <input type={'text'} value={password} name={'Password'} onChange={({target}) => setPassword(target.value)}/>
+        </div>
+        <button type={'submit'}>login</button>
+      </form>
+  )
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h1>Login:</h1>
+      {user === null ? loginForm()
+          :
+      <div>
+          <p>{user.name} logged in</p>
+          <h2>Blogs:</h2>
+          {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+          )}
+      </div>
+      }
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,16 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
+//creating token
+let token = null
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+//end of token creation
 
+//get all blogs from backend
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+export default { getAll, setToken }
